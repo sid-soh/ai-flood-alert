c4cfdab2e3f90bd4cca710ca3503d0e1d0833f34
sid-soh <99081884+sid-soh@users.noreply.github.com>
2025-09-19 17:18:33 +0800

    Added map and user location marker

--- src/main.jsx
+++ src/main.jsx
@@ -4,30 +4,6 @@ const title = (
   <h1>Live Map</h1>
 );
 
-class Car {
-  constructor(name) {
-    this.brand = name;
-  }
-
-  present() {
-    return 'I have a ' + this.brand;
-  }
-}
-
-class Model extends Car {
-  constructor(name, mod) {
-    super(name);
-    this.model = mod;
-  }  
-  show() {
-      return this.present() + ', it is a ' + this.model
-  }
-
-  hello = (val) => "Hello " + val;
-}
-
-const mycar = new Model("Ford", "Mustang");
-
 const fruitlist = ['apple', 'banana', 'cherry'];
 
 function MyList() {
@@ -40,7 +16,9 @@ function MyList() {
   );
 }
 
-
+function DisplayMap() {
+  
+}
 
 createRoot(document.getElementById('meow')).render(
   /* mycar.show() */
